3c5b5c67712e8884d5920237302b4a8b4cd74cad
Robbie Antenesse <dev@alamantus.com>
2018-03-07 13:28:31 -0700

    Make default the last 10 posts; move all to /all

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function formatItem ($, element) {
 }
 
 app.get('/', function(req, res) {
+  const maxPosts = 10;
   const url = req.query['url'];
 
   request(url, function(error, response, html) {
@@ -29,6 +30,101 @@ app.get('/', function(req, res) {
       const $ = cheerio.load(html);
 
       let items = [];
+
+      const promises = [];
+
+      const pagination = $('.pager');
+
+      if (pagination) {
+        const lastUrl = $(pagination).children().last().children().first().attr('href');
+        const pageQueryString = '?page=';
+        const pageQueryStringIndex = lastUrl.indexOf(pageQueryString);
+        const pages = lastUrl.substr(pageQueryStringIndex + pageQueryString.length);
+
+        if (pages && parseInt(pages) > 1) {
+          const numberOfPages = parseInt(pages);
+
+          const promise = new Promise(function(resolve, reject) {
+            let postsOnPage = [];
+            const pageUrl = url + '?page=' + pages;
+            request(pageUrl, function(err, r, resHtml) {
+              if (err) {
+                console.error(err);
+                reject();
+              } else {
+                const $page = cheerio.load(resHtml);
+                $page('.post').each(function (index, element) {
+                  postsOnPage.push(formatItem($page, element));
+                });
+
+                if (postsOnPage.length < maxPosts) {
+                  request(url + '?page=' + (numberOfPages - 1), function(err, r, resHtml) {
+                    const nextPage = [];
+                    $page('.post').each(function (index, element) {
+                      nextPage.push(formatItem($page, element));
+                    });
+                    postsOnPage = nextPage.concat(postsOnPage).reverse();
+
+                    for (let j = 0; j < maxPosts; j++) {
+                      items.push(postsOnPage[j]);
+                    }
+                    resolve();
+                  });
+                } else {
+                  postsOnPage = postsOnPage.reverse();
+                  for (let j = 0; j < maxPosts; j++) {
+                    items.push(postsOnPage[j]);
+                  }
+
+                  resolve();
+                }
+              }
+            });
+          });
+          promises.push(promise);
+        }
+      } else {
+        let postsOnPage = [];
+        $('.post').each(function (index, element) {
+          postsOnPage.push(formatItem($, element));
+        });
+
+        for (let i = 0; i < maxPosts; i++) {
+          if (postsOnPage[i]) {
+            items.push(postsOnPage[i]);
+          }
+        }
+
+        promises.push(Promise.resolve());
+      }
+
+      Promise.all(promises).then(function() {
+        res.set('Content-Type', 'text/xml');
+        res.send('<?xml version="1.0" encoding="UTF-8" ?>\n'
+          + '\t<rss version="2.0">\n'
+            + '\t\t<channel>\n'
+              + '\t\t\t<title>OOC</title>\n'
+              + '\t\t\t<link>' + url + '</link>\n'
+              + '\t\t\t<description>The last ' + maxPosts + ' posts</description>\n'
+              + '\t\t\t<total>' + items.length + '</total>\n'
+              + items.join('\n') + '\n'
+            + '\t\t</channel>\n'
+          + '\t</rss>\n');
+      });
+    }
+  });
+});
+
+app.get('/all', function(req, res) {
+  const url = req.query['url'];
+
+  request(url, function(error, response, html) {
+    if (error) {
+      console.error(error);
+    } else {
+      const $ = cheerio.load(html);
+
+      const items = [];
 
       $('.post').each(function (index, element) {
         items.push(formatItem($, element));
@@ -79,6 +175,7 @@ app.get('/', function(req, res) {
               + '\t\t\t<title>OOC</title>\n'
               + '\t\t\t<link>' + url + '</link>\n'
               + '\t\t\t<description>The OOC posts</description>\n'
+              + '\t\t\t<total>' + items.length + '</total>\n'
               + items.reverse().join('\n') + '\n'
             + '\t\t</channel>\n'
           + '\t</rss>\n');
