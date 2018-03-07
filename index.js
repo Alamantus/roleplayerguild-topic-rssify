1efa34b89a9e59769ecf10989d6617daebc11276
Robbie Antenesse <dev@alamantus.com>
2018-03-07 16:04:57 -0700

    Add instructions if no url is included

--- index.js
+++ index.js
@@ -24,10 +24,36 @@ function formatItem ($, element) {
       + '\t\t\t\t<dc:creator>' + (author.length > 0 ? author : '0th Post') + '</dc:creator>\n'
       + '\t\t\t\t<pubDate>' + date + '</pubDate>\n'
       + '\t\t\t\t<description>' + escape(description) + '</description>\n'
-      + '\t\t\t\t<link>https://www.roleplayerguild.com' + link + '</link>\n'
       + '\t\t\t\t<guid>https://www.roleplayerguild.com' + link + '</guid>\n'
     + '\t\t\t</item>\n';
 }
+
+const explanationHTML = `<div>
+  <p>
+    This is an app to turn topics on <a href="https://www.roleplayerguild.com" target="_blank">RolePlayerGuild</a> into an RSS
+    feed. This makes it possible to watch for changes outside of the website using other services like IFTTT.
+  </p>
+  <p>
+    Enter the URL like this:
+  </p>
+  <pre><code>https://roleplayerguild-topic-rssify.herokuapp.com/?url=[your roleplayerguild topic url]</code></pre>
+  <p>For example:</p>
+  <pre><code>https://roleplayerguild-topic-rssify.herokuapp.com/?url=https://www.roleplayerguild.com/topics/170750-guts-the-swifts-fate-rpg-system-test-run-and-workshop-session/ooc</code></pre>
+  <p>
+    <small>Notice the "/ooc" or "/ic" at the end of the url before you add it.</small>
+  </p>
+  <p>
+    By default, the top format returns the 10 most recent posts in the topic.
+  </p>
+  <p>
+    To get a feed with <em>ALL</em> of the posts, use this format:
+  </p>
+  <pre><code>https://roleplayerguild-topic-rssify.herokuapp.com/all?url=[your roleplayerguild topic url]</code></pre>
+  <p>
+    If you have any questions about it or have trouble getting this tool to work, feel free to
+    PM <a href="https://www.roleplayerguild.com/users/alamantus" target="_blank">Alamantus on RolePlayerGuild</a>.
+  </p>
+</div>`;
 
 app.get('/', function(req, res) {
   const maxPosts = 10;
@@ -35,7 +61,7 @@ app.get('/', function(req, res) {
   const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 
   if (!url) {
-    res.send('Enter the URL like this: https://roleplayerguild-topic-rssify.herokuapp.com/?url=[your roleplayerguild topic url]');
+    res.send(explanationHTML);
   } else {
     request(url, function(error, response, html) {
       if (error) {
@@ -137,71 +163,75 @@ app.get('/', function(req, res) {
 app.get('/all', function(req, res) {
   const url = req.query['url'];
 
-  request(url, function(error, response, html) {
-    if (error) {
-      console.error(error);
-    } else {
-      const $ = cheerio.load(html);
-
-      const items = [];
-
-      $('.post').each(function (index, element) {
-        items.push(formatItem($, element));
-      });
-
-      const pagination = $('.pager');
-
-      const promises = [];
-
-      if (pagination) {
-        const lastUrl = $(pagination).children().last().children().first().attr('href');
-        const pageQueryString = '?page=';
-        const pageQueryStringIndex = lastUrl.indexOf(pageQueryString);
-        const pages = lastUrl.substr(pageQueryStringIndex + pageQueryString.length);
-
-        if (pages && parseInt(pages) > 1) {
-          const numberOfPages = parseInt(pages);
-
-          for (let i = 2; i <= numberOfPages; i++) {
-            const promise = new Promise(function(resolve, reject) {
-              const pageUrl = url + '?page=' + i.toString();
-              request(pageUrl, function(err, r, resHtml) {
-                if (err) {
-                  console.error(err);
-                  reject();
-                } else {
-                  const $page = cheerio.load(resHtml);
-                  $page('.post').each(function (index, element) {
-                    items.push(formatItem($page, element));
-                  });
-
-                  resolve();
-                }
-              });
-            });
-            promises.push(promise);
-          }
-        }
+  if (!url) {
+    res.send(explanationHTML);
+  } else {
+    request(url, function(error, response, html) {
+      if (error) {
+        console.error(error);
       } else {
-        promises.push(Promise.resolve());
-      }
+        const $ = cheerio.load(html);
 
-      Promise.all(promises).then(function() {
-        res.set('Content-Type', 'text/xml');
-        res.send('<?xml version="1.0" encoding="UTF-8" ?>\n'
-          + '\t<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">\n'
-            + '\t\t<channel>\n'
-              + '\t\t\t<atom:link href="' + fullUrl + '" rel="self" type="application/rss+xml" />\n'
-              + '\t\t\t<title>' + escape(title) + '</title>\n'
-              + '\t\t\t<link>' + url + '</link>\n'
-              + '\t\t\t<description>All posts in ' + escape(title) + '</description>\n'
-              + '\t\t\t<total>' + items.length + '</total>\n'
-              + items.reverse().join('\n') + '\n'
-            + '\t\t</channel>\n'
-          + '\t</rss>\n');
-      });
-    }
-  });
+        const items = [];
+
+        $('.post').each(function (index, element) {
+          items.push(formatItem($, element));
+        });
+
+        const pagination = $('.pager');
+
+        const promises = [];
+
+        if (pagination) {
+          const lastUrl = $(pagination).children().last().children().first().attr('href');
+          const pageQueryString = '?page=';
+          const pageQueryStringIndex = lastUrl.indexOf(pageQueryString);
+          const pages = lastUrl.substr(pageQueryStringIndex + pageQueryString.length);
+
+          if (pages && parseInt(pages) > 1) {
+            const numberOfPages = parseInt(pages);
+
+            for (let i = 2; i <= numberOfPages; i++) {
+              const promise = new Promise(function(resolve, reject) {
+                const pageUrl = url + '?page=' + i.toString();
+                request(pageUrl, function(err, r, resHtml) {
+                  if (err) {
+                    console.error(err);
+                    reject();
+                  } else {
+                    const $page = cheerio.load(resHtml);
+                    $page('.post').each(function (index, element) {
+                      items.push(formatItem($page, element));
+                    });
+
+                    resolve();
+                  }
+                });
+              });
+              promises.push(promise);
+            }
+          }
+        } else {
+          promises.push(Promise.resolve());
+        }
+
+        Promise.all(promises).then(function() {
+          res.set('Content-Type', 'text/xml');
+          res.send('<?xml version="1.0" encoding="UTF-8" ?>\n'
+            + '\t<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">\n'
+              + '\t\t<channel>\n'
+                + '\t\t\t<atom:link href="' + fullUrl + '" rel="self" type="application/rss+xml" />\n'
+                + '\t\t\t<title>' + escape(title) + '</title>\n'
+                + '\t\t\t<link>' + url + '</link>\n'
+                + '\t\t\t<description>All posts in ' + escape(title) + '</description>\n'
+                + '\t\t\t<total>' + items.length + '</total>\n'
+                + items.reverse().join('\n') + '\n'
+              + '\t\t</channel>\n'
+            + '\t</rss>\n');
+        });
+      }
+    });
+  }
 });
 
 const port = process.env.hasOwnProperty('PORT') ? process.env.PORT : '5000';
